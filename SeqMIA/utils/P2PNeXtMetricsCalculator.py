b70d0ee3b16c91a877804a6955fea84e99e74fad
Marc Franz <marc.franz@iosb.fraunhofer.de>
2026-01-29 18:01:33 +0100

    You can now define suffixes of the metric names. Added Ground Truth count as an Metric Option

--- SeqMIA/utils/P2PNeXtMetricsCalculator.py
+++ SeqMIA/utils/P2PNeXtMetricsCalculator.py
@@ -9,7 +9,21 @@ import torch
 from .P2PNext_utils import process_p2pnext_output
 from .nap_and_ap_calculator import get_nap_score, get_ap_score
 from functools import partial
-from typing import Any
+from typing import Any, Optional
+
+
+def ground_truth_count(target_points: np.ndarray) -> int:
+    '''
+    Extracts the ground truth people count by counting the points inside the provided target points.
+
+    Args:
+        target_points (np.ndarray): The pixel coordinates of the actual head positions. Shape [Num_Target_Points, 2]
+    
+    Returns:
+        out (int): The actual number of people.
+    '''
+    return target_points.shape[0]
+
 
 def confidences_max(scores: np.ndarray) -> Any:
     '''
@@ -23,6 +37,7 @@ def confidences_max(scores: np.ndarray) -> Any:
     '''
     return np.max(scores, axis=0)
 
+
 def confidences_mean(scores: np.ndarray) -> Any:
     '''
     Calculates the mean value of all provided confidence scores.
@@ -34,6 +49,7 @@ def confidences_mean(scores: np.ndarray) -> Any:
         out: mean score
     '''
     return np.mean(scores, axis=0)
+
 
 def confidences_std(scores: np.ndarray) -> Any:
     '''
@@ -47,6 +63,7 @@ def confidences_std(scores: np.ndarray) -> Any:
     
     '''
     return np.std(scores, axis=0)
+
 
 def count_error(predicted_points: np.ndarray, target_points: np.ndarray) -> int:
     '''
@@ -63,6 +80,7 @@ def count_error(predicted_points: np.ndarray, target_points: np.ndarray) -> int:
     pred_count = predicted_points.shape[0]
     target_count = target_points.shape[0]
     return pred_count - target_count
+
 
 def nap(predicted_points: np.ndarray, predicted_confidence_scores: np.ndarray, 
         target_points: np.ndarray, k: int=4, threshold: float=0.5) -> float:
@@ -82,6 +100,7 @@ def nap(predicted_points: np.ndarray, predicted_confidence_scores: np.ndarray,
     nap_score, _, _ = get_nap_score(predicted_points, predicted_confidence_scores, 
                                     target_points, k, threshold)
     return nap_score
+
 
 def ap(predicted_points: np.ndarray, predicted_confidence_scores: np.ndarray,
        target_points: np.ndarray, threshold: int=8) -> float:
@@ -121,68 +140,77 @@ class MetricTypes(Enum):
 
 
 def calculate(output_raw: dict[str, torch.Tensor], target: list[dict[str, torch.Tensor]], 
-              metric_types: list[MetricTypes]) -> list[dict[str, Any]]:
+              metric_types: list[MetricTypes], model_id: Optional[str]=None) -> list[dict[str, Any]]:
     '''
     Calculate specified metrics based on the model output and target data.
+    A list of MetricTypes must be provided to specify which metrics to calculate.
 
     Args:
         output_raw (dict[str, torch.Tensor]): The raw output from the P2PNeXt model.
         target (list[dict[str, torch.Tensor]]): The ground truth target data.
         metric_types (list[MetricTypes]): A list of MetricTypes to calculate.
+        model_id (Optional[str]): Optionally set a model id string to give each metric name in the output that model id as a suffix. This is useful when you have images processed by different models and want to distinguish the metrics in the output. 
     
     Returns:
-        out (list[dict[str, Any]]): A list of dicionaries containing calculated metrics for each item in the batch.
+        out (dict[dict[str, Any]]): A dictionary of dicionaries containing calculated metrics for each item in the batch.
+        The outer dictionary takes image ids as keys. In case of JHU an image id would be something like "0432" or "0031".
+        In the inner dictionary, each metric can be accessed with the name of the metric as the key.
     '''
-    result: list[dict] = []
+    result: dict[dict[str, Any]] = {}
     processed_output = process_p2pnext_output(output_raw)
     batch_size = len(processed_output)
 
     for batch_idx in range(batch_size):
         current_pred_points, current_pred_scores = processed_output[batch_idx]
         current_target_points = target[batch_idx]["point"].numpy()
-        batch_metrics: dict = {}
+        current_image_id = target[batch_idx]["image_id"].item()
+        current_image_metrics: dict = {}
         for metric_type in metric_types:
             match metric_type:
                 case MetricTypes.MeanConfidence as mean_conf:
-                    batch_metrics[mean_conf.value[1]] = mean_conf.value[0](current_pred_scores)
+                    current_image_metrics[mean_conf.value[1]] = mean_conf.value[0](current_pred_scores)
                 
                 case MetricTypes.MaxConfidence as max_conf:
-                    batch_metrics[max_conf.value[1]] = max_conf.value[0](current_pred_scores)
+                    current_image_metrics[max_conf.value[1]] = max_conf.value[0](current_pred_scores)
                 
                 case MetricTypes.StandardDerivationConfidence as std_conf:
-                    batch_metrics[std_conf.value[1]] = std_conf.value[0](current_pred_scores)
+                    current_image_metrics[std_conf.value[1]] = std_conf.value[0](current_pred_scores)
 
                 case MetricTypes.CountError as c_err:
-                    batch_metrics[c_err.value[1]] = \
+                    current_image_metrics[c_err.value[1]] = \
                         c_err.value[0](predicted_points=current_pred_points, 
                                        target_points=current_target_points)
                 
                 case MetricTypes.NAP_K4_T05 as nap_k4_t05:
-                    batch_metrics[nap_k4_t05.value[1]] = \
+                    current_image_metrics[nap_k4_t05.value[1]] = \
                         nap_k4_t05.value[0](predicted_points=current_pred_points, 
                                             predicted_confidence_scores=current_pred_scores,
                                             target_points=current_target_points)
                 
                 case MetricTypes.NAP_K4_T01 as nap_k4_t01:
-                    batch_metrics[nap_k4_t01.value[1]] = \
+                    current_image_metrics[nap_k4_t01.value[1]] = \
                         nap_k4_t01.value[0](predicted_points=current_pred_points,
                                             predicted_confidence_scores=current_pred_scores,
                                             target_points=current_target_points)
                 
                 case MetricTypes.AP_16 as ap_16:
-                    batch_metrics[ap_16.value[1]] = \
+                    current_image_metrics[ap_16.value[1]] = \
                         ap_16.value[0](predicted_points=current_pred_points,
                                        predicted_confidence_scores=current_pred_scores,
                                        target_points=current_target_points)
                 
                 case MetricTypes.AP_8 as ap_8:
-                    batch_metrics[ap_8.value[1]] = \
+                    current_image_metrics[ap_8.value[1]] = \
                         ap_8.value[0](predicted_points=current_pred_points,
                                       predicted_confidence_scores=current_pred_scores,
                                       target_points=current_target_points)
-                
+            
+         # Append model id suffix if provided
+        if model_id is not None:
+            current_image_metrics = {f"{key}_{model_id}": value 
+                                     for key, value in current_image_metrics.items()}
         
-        result.append(batch_metrics)
+        result[current_image_id] = current_image_metrics
 
     return result
 
